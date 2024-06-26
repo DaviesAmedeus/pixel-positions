82532df05341aa5b80fe0bb11ffec571fa310914
DaviesAmedeus <amedeusedgar@gmail.com>
2024-06-26 23:28:59 +0300

    Final part1

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use App\Models\Tag;
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 
@@ -18,57 +22,45 @@ class JobController extends Controller
 
         
         return view('jobs.index',[
-            'featuredJobs'=> $jobs[0],
-            'jobs'=> $jobs[1],
+            'jobs'=> $jobs[0],
+            'featuredJobs'=> $jobs[1],
             'tags'=> Tag::all(),
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('jobs.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreJobRequest $request)
+    public function store(Request $request)
     {
-        //
+       $attributes = $request->validate([
+        'title'=>['required'],
+        'salary'=>['required'],
+        'location'=>['required'],
+        'schedule'=>['required', Rule::in(['Part Time', 'Full Time'])],
+        'url'=>['required', 'active_url'],
+        'title'=>['nullable'],
+       ]);
+
+       $attributes['featured'] = $request->has('featured');
+
+       $job = Auth::user()->employer->jobs()->create(Arr::except($attributes, 'tags'));
+
+        //Extracting the tags from the comma separated input from the form
+        if ($attributes['tags'] ?? false) {
+            foreach (explode(',', $attributes['tags']) as $tag) {
+                $job->tag($tag);
+            }
+        }
+
+        return redirect('/');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Job $job)
-    {
-        //
-    }
+  
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Job $job)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateJobRequest $request, Job $job)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Job $job)
-    {
-        //
-    }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
 
     @vite(['resources/css/app.css   ','resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-grotesk ">
+<body class="bg-black text-white font-hanken-grotesk pb-10">
 
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
@@ -27,9 +27,19 @@
                 <a href="#">Salararies</a>
         </div>
 
+            @auth
             <div>
-                <a href="">Post a Job</a>
+                <a href="/jobs/create">Post a Job</a>
             </div>
+            @endauth
+
+            @guest
+            <div class="space-x-6 font-bold">
+                <a href="/register">Sign Up</a>
+                <a href="/login">Login</a>
+            </div> 
+            @endguest
+
 
         </nav>
 
